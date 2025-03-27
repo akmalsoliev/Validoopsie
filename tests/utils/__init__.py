00c543e648574a3cba8d7b926b2d7f92588d31ec
Akmal Soliev <soliev@gmail.com>
2025-03-27 11:58:48 +0100

    improvement: spark tests time reduced by ±25% (#19)

--- tests/utils/__init__.py
+++ tests/utils/__init__.py
@@ -1,4 +1,4 @@
-from .create_frames import create_frame_fixture
+from .create_frames import create_frame_fixture, spark_session
 from .types import ReturnT
 
-__all__ = ["ReturnT", "create_frame_fixture"]
+__all__ = ["ReturnT", "create_frame_fixture", "spark_session"]
